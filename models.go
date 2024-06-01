107e5b633e174c4febf9c019290ff727ca68850f
Delayla0x <161159948+Delayla0x@users.noreply.github.com>
2024-06-01 21:35:27 +0300

    new feature

--- models.go
+++ models.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
 	"os"
-
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
 type Task struct {
+	ID          string `json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 }
+
+var tasks []Task
 
 func init() {
 	if err := godotenv.Load(); err != nil {
@@ -17,7 +23,73 @@ func init() {
 	}
 }
 
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) //Get params
+	// Loop through tasks and find one with the id from the params
+	for _, item := range tasks {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Task{})
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var task Task
+	_ = json.NewDecoder(r.Body).Decode(&task)
+	task.ID = "Some auto-generated ID" // This should be replaced with actual ID generation logic
+	tasks = append(tasks, task)
+	json.NewEncoder(w).Encode(task)
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range tasks {
+		if item.ID == params["id"] {
+			tasks = append(tasks[:index], tasks[index+1:]...)
+			var task Task
+			_ = json.NewDecoder(r.Body).Decode(&task)
+			task.ID = params["id"]
+			tasks = append(tasks, task)
+			json.NewEncoder(w).Encode(task)
+			return
+		}
+	}
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range tasks {
+		if item.ID == params["id"] {
+			tasks = append(tasks[:index], tasks[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(tasks)
+}
+
 func main() {
+	// DB connection string from .env
 	dbConnection := os.Getenv("DB_CONNECTION")
-	_ = dbConnection 
+	_ = dbConnection
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/tasks", getTasks).Methods("GET")
+	r.HandleFunc("/api/tasks/{id}", getTask).Methods("GET")
+	r.HandleFunc("/api/tasks", createTask).Methods("POST")
+	r.HandleFunc("/api/tasks/{id}", updateTask).Methods("PUT")
+	r.HandleFunc("/api/tasks/{id}", deleteTask).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
